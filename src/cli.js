01830cd991993b8a1b20a094177be13748dc3d77
derban7ikus <derbantikus@yandex.ru>
2021-10-29 16:04:36 +0300

    fixed linting mistakes

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,6 @@ import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 
-export const userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name? ');
 
+export default userName;
