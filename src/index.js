d14909b23cc3271826ef2f17139fd076261447cc
derban7ikus <derbantikus@yandex.ru>
2021-11-08 17:48:29 +0300

    index.js fix

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const gameStart = (rules, questionAndAnswer) => {
       console.log('Correct!');
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}!`);
       break;
     }
     if (round === 3) {
